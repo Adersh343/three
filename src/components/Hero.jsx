b5e0edbdb73fb0da28f0963f02bc839dbf020f51
7844Pritam <149177995+7844Pritam@users.noreply.github.com>
2024-11-18 09:18:33 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@
 
           <div>
             <h1 className={`${styles.heroHeadText} text-white`}>
-              Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+              Hi, Byteedoc<span className='text-[#915EFF]'>Coding Family</span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
               I develop 3D visuals, user <br className='sm:block hidden' />
