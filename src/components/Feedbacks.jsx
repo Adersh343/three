3edbed276793026ca690c7a1dc3b5b139f7026df
7844Pritam <pritamsharma7844@gmail.com>
2024-12-08 21:03:37 +0530

    save

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
-import { fadeIn, textVariant } from "../utils/motion";
 import { db } from "../../firebase";  // import Firebase db
 import { collection, getDocs } from "firebase/firestore";  // Firestore functions
+import { motion } from "framer-motion";  // Importing Framer Motion for animation
 
 const FeedbackCard = ({
   index,
@@ -15,7 +14,13 @@ const FeedbackCard = ({
   image,
 }) => (
   <motion.div
-    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    initial={{ opacity: 0, x: -100 }}  // Start from left side
+    animate={{ opacity: 1, x: 0 }}     // End at normal position
+    transition={{
+      type: "spring",
+      stiffness: 100,
+      delay: index * 0.2,  // Add delay for staggered animation
+    }}
     className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
   >
     <p className="text-white font-black text-[48px]">"</p>
@@ -67,10 +72,8 @@ const Feedbacks = () => {
   return (
     <div className={`mt-12 bg-black-100 rounded-[20px]`}>
       <div className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}>
-        <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>What others say</p>
-          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
-        </motion.div>
+        <p className={styles.sectionSubText}>What others say</p>
+        <h2 className={styles.sectionHeadText}>Testimonials.</h2>
       </div>
       <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
         {testimonialsData.length > 0 ? (
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,63 +4,105 @@ import { collection, getDocs } from "firebase/firestore";
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
-import { motion } from "framer-motion"; // Import motion for animations
+import { motion } from "framer-motion";
 
-// ProjectCard with animation
-const ProjectCard = ({ name, description, tags, imageUrl, githubLink, liveDemoLink }) => {
+
+const ProjectCard = ({
+  name,
+  description,
+  tags,
+  imageUrl,
+  githubLink,
+  liveDemoLink,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Function to toggle modal
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
+
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }} // Start with opacity 0 and slightly below
-      whileInView={{ opacity: 1, y: 0 }}   // Animate when in view
-      transition={{ duration: 0.6 }}    // Duration of the animation
-      viewport={{ once: true }}         // Trigger only once when the element comes into view
-      className="bg-tertiary p-5 rounded-2xl w-full"
-    >
-      <div className="relative w-full">
-        <img
-          src={imageUrl}
-          alt="project_image"
-          className="w-full h-full object-cover rounded-2xl"
-        />
-        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-          <div
-            onClick={() => window.open(githubLink, "_blank")}
-            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-          >
-            <img
-              src={github}
-              alt="source code"
-              className="w-1/2 h-1/2 object-contain"
-            />
+    <>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        viewport={{ once: true }}
+        className="bg-tertiary p-5 rounded-2xl w-full"
+      >
+        <div className="relative w-full h-56 cursor-pointer" onClick={toggleModal}
+        >
+          <img
+            src={imageUrl}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(githubLink, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="mt-5">
-        <h3 className="text-white font-bold text-[24px]">{name}</h3>
-        <p className="mt-2 text-secondary text-[14px]">{description}</p>
-      </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px] truncate">{name}</h3>
 
-      <div className="mt-4 flex flex-wrap gap-2">
-        {tags.map((tag, index) => (
-          <p key={`${name}-${index}`} className="text-[14px] text-white">
-            #{tag}
+          {/* Truncated description with ellipsis */}
+          <p
+            onClick={toggleModal}
+            className="mt-2 text-secondary text-[14px] cursor-pointer line-clamp-1"
+          >
+            {description}
           </p>
-        ))}
-      </div>
-      <div className="mt-4 flex justify-between">
-        <a
-          href={liveDemoLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500"
-        >
-          Live Demo
-        </a>
-      </div>
-    </motion.div>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag, index) => (
+            <p key={`${name}-${index}`} className="text-[14px] text-white">
+              #{tag}
+            </p>
+          ))}
+        </div>
+        <div className="mt-4 flex justify-between">
+          <a
+            href={liveDemoLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500"
+          >
+            Live Demo
+          </a>
+        </div>
+      </motion.div>
+
+      {/* Modal for full description */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="bg-tertiary p-5 rounded-2xl w-96">
+            <h3 className="text-white font-bold text-[24px]">{name}</h3>
+            <p className="mt-2 text-white text-[16px]">{description}</p>
+            <div className="mt-4 flex justify-end">
+              <button
+                onClick={toggleModal}
+                className="text-red-500 font-semibold text-[16px]"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
+
+
 
 const Works = () => {
   const [projects, setProjects] = useState([]);
@@ -114,33 +156,29 @@ const Works = () => {
       {/* Tabs for filtering */}
       <div className="flex gap-6 mb-2 mt-2">
         <button
-          className={`${
-            activeTab === "all" ? "bg-blue-500 text-white" : "bg-gray-200"
-          } py-2 px-4 rounded-full`}
+          className={`${activeTab === "all" ? "bg-blue-500 text-white" : "bg-gray-200"
+            } py-2 px-4 rounded-full`}
           onClick={() => filterProjects("all")}
         >
           All
         </button>
         <button
-          className={`${
-            activeTab === "web" ? "bg-blue-500 text-white" : "bg-gray-200"
-          } py-2 px-4 rounded-full`}
+          className={`${activeTab === "web" ? "bg-blue-500 text-white" : "bg-gray-200"
+            } py-2 px-4 rounded-full`}
           onClick={() => filterProjects("web")}
         >
           Web Apps
         </button>
         <button
-          className={`${
-            activeTab === "mobile" ? "bg-blue-500 text-white" : "bg-gray-200"
-          } py-2 px-4 rounded-full`}
+          className={`${activeTab === "mobile" ? "bg-blue-500 text-white" : "bg-gray-200"
+            } py-2 px-4 rounded-full`}
           onClick={() => filterProjects("mobile")}
         >
           Mobile Apps
         </button>
         <button
-          className={`${
-            activeTab === "other" ? "bg-blue-500 text-white" : "bg-gray-200"
-          } py-2 px-4 rounded-full`}
+          className={`${activeTab === "other" ? "bg-blue-500 text-white" : "bg-gray-200"
+            } py-2 px-4 rounded-full`}
           onClick={() => filterProjects("other")}
         >
           Others
@@ -152,10 +190,10 @@ const Works = () => {
         {filteredProjects.map((project, index) => (
           <motion.div
             key={index}
-            initial={{ opacity: 0 }} 
+            initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
-            transition={{ duration: 0.6, delay: index * 0.2 }} 
-            viewport={{ once: true }}    
+            transition={{ duration: 0.6, delay: index * 0.2 }}
+            viewport={{ once: true }}
           >
             <ProjectCard {...project} />
           </motion.div>
