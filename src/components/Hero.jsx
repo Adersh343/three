a6954be9ff9fc8b54330ad33f65a826f2b65b776
7844Pritam <pritamsharma7844@gmail.com>
2024-12-14 21:56:20 +0530

    save

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -79,7 +79,7 @@ const Hero = () => {
       </div>
 
       {/* Scroll down arrow */}
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      {/* <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
@@ -95,7 +95,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div>
+      </div> */}
 
       {/* Fixed Vertical Download Button */}
       {heroData.cvUrl && (
